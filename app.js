fd7328cdd1b30eea1c184dfcb605995ddca911e5
julioaraujo96 <49203294+julioaraujo96@users.noreply.github.com>
2022-06-12 00:12:44 +0100

    heal and special attack

--- app.js
+++ app.js
@@ -7,18 +7,46 @@ const app = Vue.createApp({
     data() {
         return {
             playerHealth:100,
-            monsterHealth:200,
+            monsterHealth:100,
+            roundsCount:0,
+        }
+    },
+    computed:{
+        monsterBar(){
+            return {width: this.monsterHealth + '%'};  
+        },
+        playerBar(){
+            return {width: this.playerHealth + '%'};
+        },
+        checkSpecialAttack(){
+            return this.roundsCount % 3 !== 0;
         }
     },
     methods: {
         attackMonster(){
-            const attackValue = getRandomValue(24,10);
+            const attackValue = getRandomValue(5,12);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+            this.roundsCount++;
+        },
+        attackPlayer(){
+            const attackValue = getRandomValue(10,20);
+            this.playerHealth -= attackValue;
+        },
+        specialAttack(){
+            this.roundsCount++;
+            const attackValue = getRandomValue(10,25);
             this.monsterHealth -= attackValue;
             this.attackPlayer();
         },
-        attackPlayer(){
-            const attackValue = getRandomValue(15,8);
-            this.playerHealth -= attackValue;
+        healPlayer(){
+            healValue = getRandomValue(5,20);
+            if (this.playerHealth + healValue > 100) {
+             return this.playerHealth = 100;
+            }
+            this.playerHealth += healValue;
         }
     }
 });
+
+app.mount(game);
